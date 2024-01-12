9c22c232d12a74882cd3fa04e1a8d2bf3cef0bf9
cosmicCode42 <>
2024-01-12 14:59:00 +0000

    created validate_data to validate the data provided, checks for exactly 6 values

--- run.py
+++ run.py
@@ -23,5 +23,23 @@ def get_sales_data():
 
     data_str = input("Enter your data here: ")
 
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers
+    Raises ValueError if strings cannot be converted into int,
+    or if there are not exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'Exactly 6 values required. You provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again.')
+
 
 get_sales_data()
